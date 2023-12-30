ec2f99d10fb2139db6320a5b6444fd2211c995f9
Jan Kodeš <jan.kodes@strv.com>
2023-12-30 12:07:58 +0100

    [fix]: Remove custom snapshot directory

--- Sources/TestKit/XCTestCase+Snapshots.swift
+++ Sources/TestKit/XCTestCase+Snapshots.swift
@@ -5,7 +5,6 @@ import SwiftUI
 public extension XCTestCase {
     func assertSnapshot<Value: View>(
         of value: @autoclosure () throws -> Value,
-        //as snapshotting: Snapshotting<Value, Format>,
         named name: String? = nil,
         record recording: Bool = false,
         timeout: TimeInterval = 5,
@@ -13,18 +12,17 @@ public extension XCTestCase {
         testName: String = #function,
         line: UInt = #line
     ) {
-        let snapshotDirectory = ProcessInfo.processInfo.environment["SNAPSHOT_REFERENCE_DIR"]! + "/" + #file
         let failure = verifySnapshot(
             of: try value(),
             as: .image,
             named: name,
             record: recording,
-            snapshotDirectory: snapshotDirectory,
             timeout: timeout,
             file: file,
             testName: testName
         )
         guard let message = failure else { return }
+
         XCTFail(message, file: file, line: line)
     }
 
